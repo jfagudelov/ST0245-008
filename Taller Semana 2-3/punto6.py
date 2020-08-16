cf0cb0e7b0f3d4d8414beadfa975af1baeaa5acc
jfagudelov <69641294+jfagudelov@users.noreply.github.com>
2020-08-16 16:08:26 -0500

    Create punto6.py

---
+++ Taller Semana 2-3/punto6.py
@@ -0,0 +1,9 @@
+def fibonacci(k):
+    if k<0:
+        print('Valor debe ser mayor a 0!')
+    elif k == 1:
+        return 0
+    elif k == 2:
+        return 1
+    else:
+        return fibonacci(k-1)+fibonacci(k-2)
