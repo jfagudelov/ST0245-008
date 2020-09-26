c5a05af15786d76d5d4b02ed080fcf4981cdcf24
jfagudelov <69641294+jfagudelov@users.noreply.github.com>
2020-09-26 16:06:52 -0500

    Update Punto_5.py

--- Taller2/Punto_5.py
+++ Taller2/Punto_5.py
@@ -1,10 +1,15 @@
 import random as rd
 
+# Se crea una lista grande con una cantidad de votos entre 101 y 1001, los cuales son o 1 para el candidato 1 o 2 para el segundo.
+
 votos = []
 votantes = rd.randint(101, 1001)
 for i in range(1,votantes):
   votos.append(rd.randint(1,2))
 
+# Se cuentan las ocurrencias de cada uno de los votos en la lista. El que tenga mayor ocurrencia, se retorna como ganador con
+# el número de votos a su favor.
+  
 def ganador_presidencia(lista_votos):
   votos_a = lista_votos.count(1)
   votos_b = lista_votos.count(2)
