0b1a2d32a5f14cc6ae926fdb71b1f9f3602898ab
jfagudelov <69641294+jfagudelov@users.noreply.github.com>
2020-09-26 16:09:33 -0500

    Update Punto_7.py

--- Taller2/Punto_7.py
+++ Taller2/Punto_7.py
@@ -1,7 +1,14 @@
+import random as rd
+
+# Se crea una lista de 50 numeros aleatorios entre -10 y 20.
+
 numeros = []
 for i in range(1,51):
   numeros.append(rd.randint(-10,20))
 
+# Se recorre cada elemento de la lista. Si el elemento es menor a 0, entonces lo agregamos a 
+# una lista con todos los negativos. Al final se retorna esta lista.
+  
 def sacar_negativos(lista):
   lista_negativos = []
   for numero in lista:
